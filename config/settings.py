d5f17fda2b48a7368123a19bab6d94524ea0a1e6
Zeehub333 <zeeninyemen@gmail.com>
2026-09-20 10:07:17 +0300

    central fx package served from odex/web/components via staticfiles

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Central UI package lives in odex/web (components/fx-package.css, ...)
+STATICFILES_DIRS = [BASE_DIR / "odex" / "web"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
